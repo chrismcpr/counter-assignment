ba839bf2c0c01aeec9b23e18bcac977a39cbfdb4
chrismcpr <chrismcoopr@gmail.com>
2024-11-20 21:59:51 -0500

    Added unit tests for Counter.js

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,38 @@
-// import necessary react testing library helpers here
-// import the Counter component here
+import { render, screen } from '@testing-library/react';
+import Counter from '../components/Counter';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 
 beforeEach(() => {
-  // Render the Counter component here
+  render(<Counter />);
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterValue = screen.getByText('0');
+  expect(counterValue).toBeInTheDocument();
+  expect(counterValue).toHaveTextContent('0');
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking + increments the count', async () => {
+  const counterValue = screen.getByTestId('count');
+  expect(counterValue).toHaveTextContent('0');
+  const increment = screen.getByRole('button', { name: '+' });
+  expect(increment).toBeInTheDocument();
+  await userEvent.click(increment);
+  const updatedCounterValue = screen.getByTestId('count');
+  expect(updatedCounterValue).toHaveTextContent('1');
+
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking - decrements the count', async () => {
+  const counterValue = screen.getByTestId('count');
+  expect(counterValue).toHaveTextContent('0');
+  await userEvent.click(screen.getByRole('button', { name: '-' }));
+  const updatedCounterValue = screen.getByTestId('count');
+  expect(updatedCounterValue).toHaveTextContent('-1');
 });
